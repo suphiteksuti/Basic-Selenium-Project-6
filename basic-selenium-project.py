4240b3919c65bb22b48f6afe52250c8c561a4b91
suphiteksuti <52538585+suphiteksuti@users.noreply.github.com>
2020-12-19 22:36:35 +0300

    Update basic-selenium-project.py

--- basic-selenium-project.py
+++ basic-selenium-project.py
@@ -11,29 +11,45 @@ browser.get("https://www.spx.com.tr")
 assert "https://www.spx.com.tr/" in browser.current_url
 
 
-toyz_and_hobies_category = browser.find_element(By.LINK_TEXT, 'Erkek')
-toyz_and_hobies_category.click()
-assert "spx.com.tr/erkek-urunleri/" in browser.current_url
+class Locators:
 
-select_product = browser.find_element(By.XPATH, '//*[@alt="Skechers Dyna Air Erkek Ayakkabı"]')
-select_product.click()
+    def category_erkek(self):
+        browser.find_element(By.LINK_TEXT, 'ERKEK').click()
 
-select_size = browser.find_element(By.CSS_SELECTOR, '#rptSecenek_ctl04_lblSCD_KOD1')
-select_size.click()
-time.sleep(3)
+    def cat_page_assert(self):
+        assert "https://www.spx.com.tr/erkek/" in browser.current_url
 
-add_to_card = browser.find_element(By.ID, 'ctl00_u19_ascUrunDetay_dtUrunDetay_ctl00_lbfbtnSepeteAt')
-add_to_card.click()
-time.sleep(3)
-assert "1 Ürün sepete eklendi." in browser.find_element(By.CSS_SELECTOR, 'div.custom-popup-header > span').text
+    def select_product(self):
+        browser.find_element(By.XPATH, '//*[@alt="Skechers Dyna Air Erkek Ayakkabı"]').click()
 
-go_to_cart_page = browser.find_element(By.LINK_TEXT, 'Sepete Git')
-go_to_cart_page.click()
-time.sleep(3)
-assert "spx.com.tr/sepet/sepetim.aspx" in browser.current_url
+    def select_size(self):
+        browser.find_element(By.CSS_SELECTOR, '#rptSecenek_ctl04_lblSCD_KOD1').click()
 
-return_homepage = browser.find_element(By.CSS_SELECTOR, '#lblBanner5758 > div > a > img')
-return_homepage.click()
+    def add_to_card(self):
+        browser.find_element(By.ID, 'ctl00_u19_ascUrunDetay_dtUrunDetay_ctl00_lbfbtnSepeteAt').click()
+
+    def add_to_card_assert(self):
+        assert "1 Ürün sepete eklendi." in browser.find_element(By.CSS_SELECTOR, 'div.custom-popup-header > span').text
+
+    def go_to_cart_page(self):
+        browser.find_element(By.LINK_TEXT, 'Sepete Git').click()
+
+    def cart_page_assert(self):
+        assert "spx.com.tr/sepet/sepetim.aspx" in browser.current_url
+
+    def return_homepage(self):
+        browser.find_element(By.CSS_SELECTOR, '#lblBanner5758 > div > a > img').click()
+
+locators = Locators()
+locators.category_erkek()
+locators.cat_page_assert()
+locators.select_product()
+locators.select_size()
+locators.add_to_card()
+locators.add_to_card_assert()
+locators.go_to_cart_page()
+locators.cat_page_assert()
+locators.return_homepage()
 time.sleep(3)
 
 browser.close()
